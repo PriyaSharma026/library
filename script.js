83ba8c8f1f0007e4d6477a3170e9bd902cbf7b12
Priya Sharma <ps9023513@gmail.com>
2025-08-30 21:09:21 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+const booksContainer = document.querySelector("#books-container");
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const readInput = document.querySelector("#read");
+const submitBtn = document.querySelector("#submit");
+
+const myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = isRead;
+}
+
+function addBookToLibrary(e, title, author, pages, isRead) {
+  e.preventDefault();
+  const newBook = new Book(title, author, pages, isRead);
+  myLibrary.push(newBook);
+  displayBooks();
+}
+
+function displayBooks() {
+  booksContainer.innerHTML = "";
+
+  myLibrary.map((book) => {
+    const bookCard = document.createElement('div')
+    const titleHeading = document.createElement("h2");
+    const authorPara = document.createElement("p");
+    const pagesPara = document.createElement("p");
+    const readPara = document.createElement("p");
+
+    titleHeading.textContent = book.title;
+    authorPara.textContent = `Author: ${book.author}`;
+    pagesPara.textContent = `Pages: ${book.pages}`;
+    readPara.textContent = `Read: ${book.read}`;
+
+    bookCard.appendChild(titleHeading);
+    bookCard.appendChild(authorPara);
+    bookCard.appendChild(pagesPara);
+    bookCard.appendChild(readPara);
+    booksContainer.appendChild(bookCard);
+
+  })
+}
+
+
+submitBtn.addEventListener("click", (e) => {
+  const isRead = readInput.checked ? "Yes" : "No";
+  addBookToLibrary(e, titleInput.value, authorInput.value, pagesInput.value, isRead);
+  titleInput.value = '';
+  authorInput.value = '';
+  pagesInput.value = '';
+  readInput.checked = false;
+})
+
